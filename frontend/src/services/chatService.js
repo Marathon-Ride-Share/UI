bde0ea0a8f54117bb8f36cfbcf0d00f03c395903
Yu Yang <yuyang2@andrew.cmu.edu>
2024-04-23 07:04:34 -0700

    finish chat functionality

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -6,21 +6,38 @@ export const createWebSocketConnection = (apiUrl, handleMessageReceived) => {
     };
 
     ws.onmessage = event => {
-        const data = JSON.parse(event.data);
-        handleMessageReceived(data);
+        const data = event.data;
+        console.log("Received raw data:", data);
+        if (typeof data === 'string') {
+            try {
+                const parsedData = JSON.parse(data);
+                console.log("Parsed data:", parsedData);
+                handleMessageReceived(parsedData);
+            } catch (error) {
+                console.error("Error parsing JSON data from WebSocket message:", error);
+            }
+        } else {
+            console.log("Data received is already an object:", data);
+            handleMessageReceived(data);
+        }
     };
+
+
 
     ws.onclose = event => {
         console.log('Connection closed');
     };
 
+
     ws.onerror = error => {
-        console.log('WebSocket encountered an error');
+        console.log('WebSocket encountered an error:', error);
+        console.log('Error details:', error.message);
         ws.close();
     };
 
     return ws;
 };
+
 
 export const sendMessage = (ws, message, recipient) => {
     if (ws && ws.readyState === WebSocket.OPEN) {

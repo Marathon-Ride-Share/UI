47d3d1dfc5cd818eeb9ae5eccd21bf1d5daac73e
Yuan Li <yuanli3@andrew.cmu.edu>
2024-04-23 04:23:29 -0700

    fix error for the synax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import WelcomePage from "./pages/WelcomePage";
 import RideShareLandingPage from "./pages/rideShare/RideShareLandingPage";
 import OriginPage from "./pages/rideShare/OriginPage";
 import DestinationPage from "./pages/rideShare/DestinationPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import CreateRidePage from "./pages/rideShare/CreateRidePage";
-<<<<<<< HEAD
+
 import RideHistoryPage from "./pages/RideHistoryPage"; // Assuming you have some global styles
 import ChatPage from "./pages/ChatPage";
-=======
+
 import PickupLocationPage from "./pages/rideShare/PickupLocationPage";
->>>>>>> 7077415 (Adding ui progress)
 
 function App() {
   const isAuthenticated = () => {
@@ -23,22 +27,22 @@ function App() {
   };
 
   return (
-      <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/welcome" element={<WelcomePage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/inride-chat" element={<ChatPage />} />
-            <Route path="/ride-share" element={<RideShareLandingPage />} />
-            <Route path="/origin" element={<OriginPage />} />
-            <Route path="/destination" element={<DestinationPage />} />
-            <Route path="/create-ride" element={<CreateRidePage />} />
-            <Route path="/ride-history" element={<RideHistoryPage />} />
-            <Route path="/pickup" element={<PickupLocationPage />} />
-            {/*<Route path="/book-ride" element={<BookRidePage />} />*/}
-            {/* Add other routes for other pages as needed */}
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/inride-chat" element={<ChatPage />} />
+        <Route path="/ride-share" element={<RideShareLandingPage />} />
+        <Route path="/origin" element={<OriginPage />} />
+        <Route path="/destination" element={<DestinationPage />} />
+        <Route path="/create-ride" element={<CreateRidePage />} />
+        <Route path="/ride-history" element={<RideHistoryPage />} />
+        <Route path="/pickup" element={<PickupLocationPage />} />
+        {/*<Route path="/book-ride" element={<BookRidePage />} />*/}
+        {/* Add other routes for other pages as needed */}
+      </Routes>
+    </Router>
   );
 }
 
